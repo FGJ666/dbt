1cee76affb859d68d3c6b986d315dca9d0b010a5
Michael <m_sozonov@outlook.com>
2026-02-21 01:18:19 +0300

    docs: добавлена документация

--- models/intermediate/flights/fct_ticket_flights.sql
+++ models/intermediate/flights/fct_ticket_flights.sql
@@ -3,19 +3,19 @@
 ) }}
 
 select
-    "stg_flights__ticket_flights"."ticket_no",
-    stg_flights__ticket_flights.flight_id,
-    stg_flights__ticket_flights.fare_conditions,
-    stg_flights__ticket_flights.amount,
+    "ticket_flights"."ticket_no",
+    ticket_flights.flight_id,
+    ticket_flights.fare_conditions,
+    ticket_flights.amount,
     case
-        when "stg_flights__boarding_passes"."ticket_no" is null then 'no'
+        when "boarding_passes"."ticket_no" is null then 'no'
         else 'yes'
     end as boarding_pass_exists,
-    "stg_flights__boarding_passes"."ticket_no" as boarding_no,
-    "stg_flights__boarding_passes"."seat_no" as seat_no,
+    "boarding_passes"."ticket_no" as boarding_no,
+    "boarding_passes"."seat_no" as seat_no,
     current_date as load_date
 from
-    {{ ref('stg_flights__ticket_flights') }}
-    left join {{ ref('stg_flights__boarding_passes') }}
-    on "stg_flights__ticket_flights"."ticket_no" = "stg_flights__boarding_passes"."ticket_no"
-    and "stg_flights__ticket_flights"."flight_id" = "stg_flights__boarding_passes"."flight_id"
+    {{ ref('stg_flights__ticket_flights') }} as ticket_flights
+    left join {{ ref('stg_flights__boarding_passes') }} as boarding_passes
+    on "ticket_flights"."ticket_no" = "boarding_passes"."ticket_no"
+    and "ticket_flights"."flight_id" = "boarding_passes"."flight_id"
