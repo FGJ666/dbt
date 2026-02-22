d4450ab98e47e09e697ee4982b5c419b922d1dac
Michael <m_sozonov@outlook.com>
2026-02-23 01:21:41 +0300

    test: добавлены разные способы материализации

---
+++ models/staging/flights/stg_flights__bookings_append.sql
@@ -0,0 +1,23 @@
+{{ config(
+  materialized = 'incremental',
+  incremental_strategy = 'append'
+) }}
+
+select
+    book_ref,
+    book_date,
+    total_amount
+from
+  {{ source(
+    'demo_src',
+    'bookings'
+  ) }}
+
+{% if is_incremental() %}
+    where
+        book_ref > (
+            select max(book_ref)
+            from
+                {{ this }}
+        )
+{% endif %}
