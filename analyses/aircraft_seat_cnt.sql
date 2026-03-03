3f36f712d38a1044a1a312729c21ecec87dcadda
Michael <m_sozonov@outlook.com>
2026-03-03 23:49:00 +0300

    adhoc: решена задача

---
+++ analyses/aircraft_seat_cnt.sql
@@ -0,0 +1,15 @@
+select
+    aircrafts.aircraft_code,
+    aircrafts.model,
+    count(
+        distinct seats.seat_no
+    ) as seat_count
+from
+    {{ ref('stg_flights__seats') }} as seats
+    inner join {{ ref('stg_flights__aircrafts') }} as aircrafts
+    on seats.aircraft_code = aircrafts.aircraft_code
+group by
+    aircrafts.aircraft_code,
+    aircrafts.model
+order by
+    seat_count desc;
