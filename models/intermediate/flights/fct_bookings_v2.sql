b070e808f367e83698913da85c67015f6df4d1ce
Michael <m_sozonov@outlook.com>
2026-03-30 19:48:30 +0300

    feat: удалены версии модели

--- models/intermediate/flights/fct_bookings_v2.sql
+++
@@ -1,6 +0,0 @@
-select
-    {{ dbt_utils.generate_surrogate_key(['book_ref']) }} as book_ref_key,
-    {{ show_columns_relation('stg_flights__bookings') -}},
-    current_date as loaded_at
-from
-    {{ ref('stg_flights__bookings') }}
