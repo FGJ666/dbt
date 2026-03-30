07e12c35bb05e1d5959fd88ce7d3678ffa675a14
Michael <m_sozonov@outlook.com>
2026-03-31 00:00:23 +0300

    feat: добавлены unit тесты в модель stg_flights__flights

--- models/staging/flights/stg_flights__flights.sql
+++ models/staging/flights/stg_flights__flights.sql
@@ -14,9 +14,15 @@ from
     'demo_src',
     'flights'
   ) }}
-where
-    scheduled_departure > (
-        select max(scheduled_departure)
-        from
-            {{ this }}
-    ) - interval '100 day'
+
+{% if is_incremental() %}
+    -- Логика инкрементального обновления:
+    -- 1. Добавляем новые рейсы с scheduled_departure >= максимальной даты в таблице
+    -- 2. Обновляем существующие рейсы по unique_key (flight_id)
+    where scheduled_departure >= (
+        select max(scheduled_departure) from {{ this }}
+    )
+    or flight_id in (
+        select flight_id from {{ this }}
+    )
+{% endif %}
