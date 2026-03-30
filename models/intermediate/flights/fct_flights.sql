355f2e35ed6028763428155a0b22fa5469dfe8a6
Michael <m_sozonov@outlook.com>
2026-03-30 19:49:51 +0300

    feat: добавлено поле actual_duration_flight

--- models/intermediate/flights/fct_flights.sql
+++ models/intermediate/flights/fct_flights.sql
@@ -9,6 +9,11 @@ select
     aircraft_code,
     actual_departure,
     actual_arrival,
+    case
+        when actual_departure is not NULL and actual_arrival is not NULL
+            then actual_arrival - actual_departure
+        else INTERVAL '0 seconds'
+    end as actual_duration_flight,
     current_date as load_date
 from
     {{ ref('stg_flights__flights') }}
